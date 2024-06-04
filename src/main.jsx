5a87c544d7fe40e35a6bc4b333555f96b677fdda
iraiyan50 <128201651+iraiyan50@users.noreply.github.com>
2024-06-04 11:00:04 +0600

    paths reload

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,43 @@ import './index.scss'
 import { store } from './store/store'
 import { Provider } from 'react-redux'
 
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/PageNotFound";
+
+const router = createBrowserRouter([
+  {
+    path: "/devmood/",
+    element: <App />,
+    children: [
+      {
+        path: "/devmood/",
+        element: <Home />,
+      },
+      {
+        path: "/:mediaType/:id",
+        element: <Details />,
+      },
+      {
+        path: "/search/:query",
+        element: <SearchResult />,
+      },
+      {
+        path: "/explore/:mediaType",
+        element: <Explore />,
+      },
+      {
+        path: "*",
+        element: <PageNotFound />,
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
